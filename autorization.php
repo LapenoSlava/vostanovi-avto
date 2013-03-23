18faa366453446289a3fbffd0e2477d20b274786
LapenoSlava <loktik.v@gmail.com>
2013-03-23 19:37:09 +0400

    some changes

---
+++ autorization.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Метод GET</title>
+</head>
+<body>
+    <form action = "autorizer.php" method = "post">
+        <p><b>Welcome to the future!</b></p>
+        <p>
+            <input type="textbox" name="email" maxlength="30"><br>
+            <input type="textbox" name="password" maxlength="15"><br>
+        </p>
+        <p><input type="submit" value="Sign in/up"></p>
+    </form>
+</body>
+</html>
